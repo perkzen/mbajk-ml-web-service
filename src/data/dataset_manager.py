8ced8031c039d09cab2c12af5949ab2eb0ed09f6
perkzen <perko.domen@gmail.com>
2024-03-10 20:32:39 +0100

    feat: add dataset_manager.py

---
+++ src/data/dataset_manager.py
@@ -0,0 +1,22 @@
+import os
+import pandas as pd
+
+from entities import BikeStationWithWeather
+
+
+class DatasetManager:
+
+    def __init__(self, data_path: str):
+        self.data_path = data_path
+
+    def save(self, station: BikeStationWithWeather):
+        filename = f"{self.data_path}/mbajk_dataset_{station.name}.csv"
+
+        if os.path.exists(filename):
+            existing_df = pd.read_csv(filename)
+            df = pd.DataFrame([station.dict()])
+            df = pd.concat([existing_df, df], ignore_index=True)
+            df.to_csv(filename, index=False)
+        else:
+            df = pd.DataFrame([station.dict()])
+            df.to_csv(filename, index=False)
--- src/data/fetch_data.py
+++ src/data/fetch_data.py
@@ -2,10 +2,10 @@ import signal
 import sys
 import time
 import schedule
-import pandas as pd
 from datetime import datetime
 
 from data_fetcher import DataFetcher
+from dataset_manager import DatasetManager
 
 
 def signal_handler(_sig, _frame):
@@ -15,20 +15,17 @@ def signal_handler(_sig, _frame):
     sys.exit(0)
 
 
-def task(data_fetcher: DataFetcher):
+def task(data_fetcher: DataFetcher, dataset_manager: DatasetManager):
     start_time = datetime.now()
 
-    print(f"[Task]: Fetching bike stations - {start_time.strftime('%Y-%m-%dT%H:MM')}", )
+    print(f"[Task]: Fetching bike stations - {start_time.strftime('%Y-%m-%dT%H:%M')}", )
 
     stations = data_fetcher.get_stations_with_weather()
 
-    stations = [station.dict() for station in stations]
-
-    df = pd.DataFrame(stations)
-
-    timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-
-    df.to_csv(f"data/raw/mbajk_dataset_{timestamp}.csv", index=False)
+    for station in stations:
+        # this condition is for development purposes
+        if station.name == "GOSPOSVETSKA C. - TURNERJEVA UL.":
+            dataset_manager.save(station)
 
     end_time = datetime.now()
     execution_time = end_time - start_time
@@ -40,8 +37,9 @@ signal.signal(signal.SIGINT, signal_handler)
 
 if __name__ == "__main__":
     fetcher = DataFetcher(lat=46.5547, lon=15.6467)
+    manager = DatasetManager(data_path="data/raw")
 
-    schedule.every(1).hour.do(task, fetcher)
+    schedule.every(2).seconds.do(task, fetcher, manager)
 
     while True:
         schedule.run_pending()
