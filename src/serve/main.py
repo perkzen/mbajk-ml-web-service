360c91851962db8088099a41abcfa28758ce5442
perkzen <perko.domen@gmail.com>
2024-03-13 22:50:07 +0100

    feat: add cors

--- src/serve/main.py
+++ src/serve/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from starlette.responses import RedirectResponse
-
 from .routers import health_router, bike_stations_router, prediction_router
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(health_router)
 app.include_router(bike_stations_router)
