19ddc5fb2bd4cab5df17c90cbe835e271e37130f
perkzen <perko.domen@gmail.com>
2024-03-17 16:33:28 +0100

    feat: add skeleton loading

--- src/client/.prettierrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  tabWidth: 2,
-  semi: true,
-  trailingComma: 'es5',
-  singleQuote: true,
-  printWidth: 80,
-  plugins: ['prettier-plugin-tailwindcss'],
-};
--- src/client/src/components/map/info-window/bike-station-info-window.tsx
+++ src/client/src/components/map/info-window/bike-station-info-window.tsx
@@ -4,7 +4,7 @@ import { useQueryParams } from '@/lib/hooks/use-querey-params';
 import { timeSince } from '@/lib/utils';
 import { Bike, ParkingSquare, X } from 'lucide-react';
 import PredictionsTable from '@/components/map/info-window/predictions-table';
-import LoadingProvider from '@/components/providers/loading-provider';
+import { Skeleton } from '@/components/ui/skeleton';
 
 
 interface BikeStationInfoWindowProps {
@@ -14,7 +14,7 @@ interface BikeStationInfoWindowProps {
 const BikeStationInfoWindow = ({ onClose }: BikeStationInfoWindowProps) => {
   const { urlQuery } = useQueryParams();
 
-  const { data, isLoading, isFetching } = useBikeStationByNumber(Number(urlQuery.station), {
+  const { data, isLoading } = useBikeStationByNumber(Number(urlQuery.station), {
     enabled: Number(urlQuery.station) > 0,
   });
 
@@ -28,52 +28,60 @@ const BikeStationInfoWindow = ({ onClose }: BikeStationInfoWindowProps) => {
   return (
     <DrawerContent
       className="flex flex-col rounded-t-[10px] h-full  w-full sm:w-[400px] fixed top-20  bottom-0 left-0">
-      <LoadingProvider isLoading={isLoading || isFetching}>
-        <div className="mx-auto w-full max-w-sm">
-          <DrawerHeader>
-            <div className={'flex flex-row'}>
-              <DrawerTitle className={'flex flex-row text-wrap'}>
-                <div className={'leading-6'}>
-                  {data?.name}
-                  <span className={'font-light text-gray-400'}> - n&apos;{data?.number}</span>
-                </div>
-              </DrawerTitle>
-              <DrawerClose onClick={onClose} className={'ml-auto self-start'}>
-                <X width={16} height={16} />
-              </DrawerClose>
-            </div>
-            <DrawerDescription className={'text-start'}>Last
-              updated: {timeSince(Number(data?.last_updated))}</DrawerDescription>
-          </DrawerHeader>
-          <div className="flex flex-col p-4 gap-8">
-            <div className="flex flex-row gap-4">
-
-
-              <div className="flex flex-col gap-2">
-                <div className={'flex flex-row items-center gap-2'}>
-                  <Bike size={24} />
-                  <div className="text-xl font-semibold text-primary">{data?.available_bikes}</div>
-                </div>
-                <div className="text-gray-400">Available bikes</div>
+      <div className="mx-auto w-full max-w-sm">
+        <DrawerHeader>
+          <div className={'flex flex-row'}>
+            <DrawerTitle className={'flex flex-row text-wrap'}>
+              <div className={'leading-6'}>
+                {isLoading ? <Skeleton className="w-[300px] h-[20px] rounded-md" />
+                  :
+                  <>
+                    {data?.name}
+                    <span className={'font-light text-gray-400'}> - n&apos;{data?.number}</span>
+                  </>}
               </div>
+            </DrawerTitle>
+            <DrawerClose onClick={onClose} className={'ml-auto self-start'}>
+              <X width={16} height={16} />
+            </DrawerClose>
+          </div>
+          <DrawerDescription className={'text-start'}>
+            {isLoading ? <Skeleton className="w-[100px] h-[20px] rounded-md" /> :
+              <>
+                Last updated: {timeSince(Number(data?.last_updated))}
+              </>}
+          </DrawerDescription>
+        </DrawerHeader>
+        <div className="flex flex-col p-4 gap-8">
+          <div className="flex flex-row gap-4">
 
 
-              <div className="flex flex-col gap-2">
-                <div className={'flex flex-row items-center gap-2'}>
-                  <ParkingSquare size={24} />
-                  <div className="text-xl font-semibold text-primary">{data?.available_bike_stands}</div>
-                </div>
-                <div className="text-gray-400">Available stands</div>
+            <div className="flex flex-col gap-2">
+              <div className={'flex flex-row items-center gap-2'}>
+                <Bike size={24} />
+                {isLoading ? <Skeleton className="w-[32px] h-[28px] rounded-md" /> :
+                  <div className="text-xl font-semibold text-primary">{data?.available_bikes}</div>}
               </div>
-
+              <div className="text-gray-400">Available bikes</div>
             </div>
 
 
-            <PredictionsTable data={predictions || []} isLoading={isLoadingPredictions} />
+            <div className="flex flex-col gap-2">
+              <div className={'flex flex-row items-center gap-2'}>
+                <ParkingSquare size={24} />
+                {isLoading ? <Skeleton className="w-[32px] h-[28px] rounded-md" /> :
+                  <div className="text-xl font-semibold text-primary">{data?.available_bike_stands}</div>}
+              </div>
+              <div className="text-gray-400">Available stands</div>
+            </div>
 
           </div>
+
+
+          <PredictionsTable data={predictions || []} isLoading={isLoadingPredictions} />
+
         </div>
-      </LoadingProvider>
+      </div>
     </DrawerContent>
   );
 };
