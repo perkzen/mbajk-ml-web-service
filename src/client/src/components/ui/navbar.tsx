1f8d2ac286955447e56e9702f62156a6a022d836
perkzen <perko.domen@gmail.com>
2024-03-18 12:16:08 +0100

    fea: update models

--- src/client/src/components/ui/navbar.tsx
+++ src/client/src/components/ui/navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     <nav className={'flex flex-row w-full py-4 px-8 shadow-lg'}>
       <div className={'flex flex-row items-center gap-2 rounded-full bg-primary w-fit py-1 px-3'}>
         <Image src={Logo} alt={'MBajk AI'} width={24} height={24} className={'rounded-full'} />
-        <div className={'text-white text-lg font-semibold'}>mbajk ejaj</div>
+        <div className={'text-white text-lg font-semibold'}>mbajk AI</div>
       </div>
       <div className={'ml-auto bg-black h-fit rounded-full p-2'}>
         <Link target="_blank" href={'https://github.com/perkzen/mbajk-ml-web-service'}>
