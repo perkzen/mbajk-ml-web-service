92f9c77f234742906d6a9acdf9130fd0227730ee
perkzen <perko.domen@gmail.com>
2024-03-31 18:17:12 +0200

    fix: job

--- src/data/data_manager.py
+++ src/data/data_manager.py
@@ -12,31 +12,16 @@ class DataManager:
 
     def __init__(self, data_path: str):
         self.data_path = data_path
-        dagshub.auth.add_app_token(settings.dagshub_user_token)
 
     def save(self, folder: str, file_name: str, df: pd.DataFrame,
              override: bool = False) -> None:
         file_path = f"{self.data_path}/{folder}/{file_name}.csv"
-
-        # s3 = get_repo_bucket_client(settings.repo_name)
-        #
-        # s3.download_file(
-        #     Bucket=settings.bucket_name,
-        #     Key=file_path,
-        #     Filename=file_path,
-        # )
 
         if os.path.exists(file_path) and not override:
             existing_df = pd.read_csv(file_path)
             df = pd.concat([existing_df, df], ignore_index=True)
 
         df.to_csv(file_path, index=False)
-
-        # s3.upload_file(
-        #     Bucket=settings.bucket_name,
-        #     Filename=file_path,
-        #     Key=file_path,
-        # )
 
     def get_dataframe(self, folder: str, file_name: str) -> pd.DataFrame:
         file_path = f"{self.data_path}/{folder}/{file_name}.csv"
