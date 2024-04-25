a0ea3ddbb3e296a5d7bf6aaad4737d40b5388cb0
perkzen <perko.domen@gmail.com>
2024-04-25 21:16:11 +0200

    fix: great expectations

--- src/data/scripts/run_checkpoint.py
+++ src/data/scripts/run_checkpoint.py
@@ -9,14 +9,9 @@ def main():
     result: CheckpointResult = context.run_checkpoint(checkpoint_name="mbajk_checkpoint")
 
     if not result["success"]:
-        print("[Validate]: Checkpoint validation failed!")
-       # raise ValueError("[Validate]: Checkpoint validation failed!")
+        raise ValueError("[Validate]: Checkpoint validation failed!")
     else:
         print("[Validate]: Checkpoint validation passed!")
-
-    dm = DataManager(data_path="data")
-    current = dm.get_dataframe("processed", "current_data")
-    dm.save("processed", "reference_data", current, override=True)
 
 
 if __name__ == "__main__":
