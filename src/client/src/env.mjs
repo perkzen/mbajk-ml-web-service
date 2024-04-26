3e5b214a2133c182e441056f6958256ce122f044
perkzen <perko.domen@gmail.com>
2024-04-26 17:51:09 +0200

    fix: docker file client

--- src/client/src/env.mjs
+++ src/client/src/env.mjs
@@ -17,4 +17,5 @@ export const env = createEnv({
     NEXT_PUBLIC_GOOGLE_MAPS_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY,
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
+  skipValidation: process.env.SKIP_ENV_VALIDATION === 'true',
 });
