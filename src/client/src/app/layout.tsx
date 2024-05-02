52bae74084c6c5fef7e915c9d588efe1ed3da10f
perkzen <perko.domen@gmail.com>
2024-05-02 16:19:18 +0200

    feat: add metadata

--- src/client/src/app/layout.tsx
+++ src/client/src/app/layout.tsx
@@ -12,6 +12,21 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'MBajk AI',
   description: 'MBajk AI is a smarter way to use Mbjak',
+  authors: { url: 'https://www.domenperko.com/', name: 'Domen Perko' },
+  keywords: ['MBajk', 'AI'],
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: process.env.NEXT_PUBLIC_SITE_URL,
+    images: [
+      {
+        url: `${process.env.NEXT_PUBLIC_SITE_URL}/og-image.png`,
+        width: 1200,
+        height: 630,
+        alt: 'MBajk AI',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
--- src/client/src/middleware.ts
+++ src/client/src/middleware.ts
@@ -2,8 +2,8 @@ import {NextResponse} from 'next/server'
 import type {NextRequest} from 'next/server'
 
 export const config = {
-    matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
-}
+    matcher: ['/((?!api|_next/static|_next/image|favicon.ico|og-image.png).*)'],
+};
 
 export function middleware(request: NextRequest) {
     if (request.url !== '/') {
